bea6c034823a89fedc798dead7b8a9bb3c186c18
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2024-01-07 16:46:19 +0100

    (init): project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1 className={`text-6xl text-black`}>Welcome to ApolloChat</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
