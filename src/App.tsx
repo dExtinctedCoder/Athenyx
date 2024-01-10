285286b30cb15698e0253b0a0f18dc4e2750cc45
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2024-01-10 02:16:33 +0100

    wrap root component in required context wrappers and add routes component to jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import RoutesIndex from './routes'
+import { Suspense } from 'react'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from './utils/constants'
+import { AppGlobalContext } from './components/context/global'
+
+function Loader() {
+  return (<div>Loading...</div>)
+}
+
 function App() {
   return (
     <>
-      <div className="app">
-        <h1 className={`text-6xl text-black`}>Welcome to ApolloChat</h1>
+      <div className="app bg-[#F9F9F9] min-h-[100dvh] text-[#222]">
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={Loader()}>
+            <AppGlobalContext>
+              <RoutesIndex />
+            </AppGlobalContext>
+          </Suspense>
+        </QueryClientProvider>
       </div>
     </>
   )
