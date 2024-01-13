ce08aab4f84c88d0d616bc140fabdc15e044439a
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2024-01-13 20:35:50 +0100

    feat: add homebar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import Homebar from '../components/utils/homebar'
+
 const LandingPage = () => {
   return (
     <div>
       <h1>Welcome to Apollochat<br />Landing Page</h1>
+      <Homebar />
     </div>
   )
 }
